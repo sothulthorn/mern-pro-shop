8f87159c91834e327998880db22e69f1c81357f1
sothulthorn <thornsothul@gmail.com>
2024-02-03 21:19:28 +0900

    Product Controller

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -17,7 +17,7 @@ const HomeScreen = () => {
 
   return (
     <>
-      <h1>Lastes Products</h1>
+      <h1>Lastest Products</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
