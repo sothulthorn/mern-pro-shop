00e9733444db28da88f164e898c12f6a86b501fd
sothulthorn <thornsothul@gmail.com>
2024-02-04 20:37:59 +0900

    JWT HTTP Only Cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,28 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 // @desc    Auth user & get the token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
+    // Create Json Web Token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    });
+
+    // Set JWT as HTTP-Only cookie
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
+    });
+
     res.json({
       _id: user._id,
       name: user.name,
