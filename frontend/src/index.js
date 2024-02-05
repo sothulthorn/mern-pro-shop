4698082454fbc9482d9d9afee193dbc68a6d68ee
sothulthorn <thornsothul@gmail.com>
2024-02-05 21:30:11 +0900

    Private Routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import store from './store';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
+import PrivateRoute from './components/PrivateRoute';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CardScreen from './screens/CardScreen';
@@ -27,7 +28,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CardScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
