ce472c5b3fed8d73a522bbd4cb6ec15249983766
sothulthorn <thornsothul@gmail.com>
2024-02-09 09:01:15 +0900

    Paginate Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,9 +3,15 @@ import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
+import { useParams } from 'react-router-dom';
 
 const HomeScreen = () => {
-  const { data: products, isLoading, isError: error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const {
+    data,
+    isLoading,
+    isError: error,
+  } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -19,7 +25,7 @@ const HomeScreen = () => {
         <>
           <h1>Lastest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
